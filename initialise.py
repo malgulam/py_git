fe630a520de5b32702d763bccc7cd83a9a4dbe6f
druzgeorge <malimbageorgeyandanbon@gmail.com>
2020-09-20 16:25:24 +0100

    fixed abs path problem

--- initialise.py
+++ initialise.py
@@ -1,10 +1,12 @@
-#this file must be called first at the
-#start of the program
-#it is the initialisation class that sets-up the begining of this vc
+# this file must be called first at the
+# start of the program
+# it is the initialisation class that sets-up the begining of this vc
 
-#imports
+# imports
 import os
 import shelve
+
+
 # orginalDirs = list()
 class initialise:
     def __init__(self):
@@ -12,7 +14,7 @@ class initialise:
 
     def start(self, cwd):
 
-        #creating cache dirs
+        # creating cache dirs
         self.cwd = cwd
         print(self.cwd)
         cacheDir = "CacheDir/"
@@ -20,29 +22,30 @@ class initialise:
             pass
         else:
             os.mkdir(os.path.join(cwd, cacheDir))
-        #Creating db for caching the current dirs and filenames
+        # Creating db for caching the current dirs and filenames
         cacheFile = "CachedDB"
-        dirsCache = initialise.dirCacher(self, cwd=self.cwd) #tODO: Set return to list
-        filesCache = initialise.filesCacher(self, cwd=self.cwd) #tODO: Set return to list
+        dirsCache = initialise.dirCacher(self, cwd=self.cwd)
+        filesCache = initialise.filesCacher(self, cwd=self.cwd)
         dirsCacheFile = "dirCacheFile"
-        #saving dir cacher to shelf file
-        Dirshelve = shelve.open(os.path.join(cwd, str(cacheDir)+str(cacheFile)))
-        Dirshelve['dirs'] = dirsCache #todo: saving the list to  the file
+        # saving dir cacher to shelf file
+        Dirshelve = shelve.open(os.path.join(cwd, str(cacheDir) + str(cacheFile)))
+        Dirshelve['dirs'] = dirsCache
         Dirshelve.close()
-        FilesShelve = shelve.open(os.path.join(cwd, str(cacheDir)+str(cacheFile)))
+        FilesShelve = shelve.open(os.path.join(cwd, str(cacheDir) + str(cacheFile)))
         FilesShelve['files'] = filesCache
         FilesShelve.close()
-        #todo: work on caching file content
+        # todo: work on caching file content
+
     def dirCacher(self, cwd):
         self.cwd = cwd
         dirs = list()
         for root, dir, files in os.walk(self.cwd, topdown=False):
             dirs.append(dir)
         return dirs
+
     def filesCacher(self, cwd):
         cwd = self.cwd
         filenames = list()
         for root, dir, file in os.walk(self.cwd, topdown=False):
             filenames.append(file)
         return filenames
-

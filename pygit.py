ea9f0bfe1c2263d86938983a1d9f17264d8f36d2
druz <druz@localhost.localdomain>
2020-10-02 09:06:26 -0400

    new

---
+++ pygit.py
@@ -0,0 +1,12 @@
+#imports
+
+#pygit main class
+class pygit(object)
+    def __init__(self):
+        self = self
+    @classmethod
+    def clone(cls, origin_uri):
+        pass
+    @staticmethod
+    def initialise(self, cwd):
+        

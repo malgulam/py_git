08cd41091fd4f636dd3cb016f417a7da7423957e
druz <druz@localhost.localdomain>
2020-10-10 20:38:30 -0400

    new changes.Pushed with automate_actions

--- pygit/pygit.py
+++ pygit/pygit.py
@@ -11,20 +11,18 @@ import shutil
 
 BASE_DIR = Path.home()
 DESKTOP = BASE_DIR / 'Desktop'
-SHELF_DIR = BASE_DIR / 'python-git-shelf'
+SHELF_DIR = str(BASE_DIR) +'/'+ 'python-git-shelf'
 
 #todo: fix winows support
 #initialise the program
 def initialise():
     print('Initialising....')
-    #todo: search whether the git shelf is empty before doing all this to prevent annoying prompts
     #check if system supports git
     osType = detect_os()
     if not git_exists(osType):
         sys.exit(1)
     print('System supports git!')
     args = get_commandline_arguments()
-    #todo: if you make changes to commands list make changes to self.commands in the recognize_commands in class Commands:
     push = args.push
     set_global_credentials = args.set_global_credentials
     masterDir = args.masterDir
@@ -88,8 +86,7 @@ def get_commandline_arguments():
 
 
 
-#todo: create commands to deal with command related issues
-
+#Commands Class
 class Commands(object):
     def __eq__(self, other):
         if isinstance(other, self.__class__):
@@ -107,12 +104,9 @@ class Commands(object):
         self.argument = argument
         self.argument_content = argument_content
         print(self.argument, self.argument_content)
-        #below is a list of k,v items in a commands dict which maps each command to it's respectiive function!
-        # self.commands = {'masterDir':"index_master()", "automate_actions":"automate_actions()", "clone":"clone()", "init":"init()", "push":"push()", "set_global_credentials":"set_globals()"}
-        #todo: redirect every argument to it's static function
         #redirecting to respective function
         if self.argument == 'masterDir':
-            Commands.index_master()
+            Commands.index_master(self.argument_content)
         elif self.argument == 'automate_actions':
             Commands.automate_actions(action=self.argument_content)
         elif self.argument == 'clone':
@@ -140,9 +134,8 @@ class Commands(object):
             return "Unknown action {}".format(action)
     @staticmethod
     def set_globals(username_password):
-        # todo: add warning message to set_globals showing this option is not secure
-        # todo: let user pass passwords and username like 'username/password' so you can use .split(/) to
-        # todo: retrive both username and password from a declared list
+        #todo: fix yes or no query
+        #todo: fix b'globalcredentials' KeyError problem
         #spliting username_password into a list separated by ','
         credentials = str(username_password).split('/')
         #making sure coloit will be usable in commandline interfaces
@@ -151,23 +144,20 @@ class Commands(object):
                           f"{SHELF_DIR} as a shelve file.\nYou can proceed but it is advised to "
                           f"setup SSH keys for your github to avoid using this.\nRead this:{color_front('https://docs.github.com/en/free-pro-team@latest/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account', red=0, green=255, blue=0)}"
                           , red=255, green=0, blue=0))
-        opt = str(input(f"\n\nDO YOU WISH TO PROCEED? y(Y)es/n(N)o: ")).lower()
-        # print(opt.lower())
+        opt = input(f"\n\nDO YOU WISH TO PROCEED? y(Y)es/n(N)o: ")
+        opt = opt.lower()
         if opt == "y" or "yes":
             print("Working...")
             try:
                 Commands.safe_mkdir(SHELF_DIR)
-                shelveOBJ = shelve.open(SHELF_DIR)
-                shelveOBJ['gloabal_credentials'] = credentials[0]
-                shelveOBJ['global_credentials'].append(credentials[1])
-                shelveOBJ.close()
+                print('credentials', credentials)
+                # so password is gloablcredentilas[1] and username is [0]
+                Commands.shelfer(key='global_credentials', content=credentials)
             except FileExistsError:
                 shutil.rmtree(SHELF_DIR)
                 Commands.safe_mkdir(SHELF_DIR)
-                shelveOBJ = shelve.open(SHELF_DIR)
-                shelveOBJ['gloabal_credentials'] = credentials[0]
-                shelveOBJ['global_credentials'].append(credentials[1])
-                shelveOBJ.close()
+                # so password is gloablcredentilas[1] and username is [0]
+                Commands.shelfer(key='global_credentials', content=credentials)
             print("Done...")
             return
         else:
@@ -175,15 +165,26 @@ class Commands(object):
             print(color_front("Read: https://docs.github.com/en/free-pro-team@latest/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account \n On how to perform adding SSH key to account", red=0, green=150, blue=240))
             sys.exit(1)
     @staticmethod
-    def init():
-        #todo: set path to cwd
-        os.system()
+    def init(cwd):
+        os.chdir(cwd)
+        os.system("git init")
+        print("initialised git!")
+        return
     @staticmethod
-    def index_master():
-        pass
+    def index_master(path_to_master_local_repo):
+        os.chdir(path_to_master_local_repo)
+        Commands.init(cwd=os.getcwd())
+        Commands.shelfer(key="master_repo", content=path_to_master_local_repo)
+        print('MasterIndexed')
+        return
     @staticmethod
     def clone(url, path=DESKTOP):
+        print("Cloning to Desktop")
+        cwd = os.getcwd()
+        os.chdir(path)
+        os.chdir(cwd)
         os.system("git clone {}".format(url))
+        return
     @staticmethod
     def push(path):
         print("Pushing new code")
@@ -198,6 +199,13 @@ class Commands(object):
         shutil.rmtree(path)
         os.mkdir(path)
         print("Directory successfully created at {}".format(path))
+        return
+    @staticmethod
+    def shelfer(key="masterKey", content=None):
+        shelve_file_path = os.path.join(str(SHELF_DIR), 'pygit_shelve')
+        shelfobj = shelve.open(shelve_file_path)
+        shelfobj[key] = content
+        shelfobj.close()
         return
 if __name__ == '__main__':
     initialise()

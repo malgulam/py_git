db049a117901c2c77642f85de148ef5f16a3b6fa
druz <druz@localhost.localdomain>
2020-10-11 14:26:56 -0400

    new changes.Pushed with automate_actions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="py_git",
-    version=0.2,
+    version=0.1,
     author="druzgeorge",
     description="Essentially: Pythonically Git.",
     long_description = long_description,
